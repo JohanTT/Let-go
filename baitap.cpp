a795e09acfb1c54668bb94335e80c372c7fff3b1
Kirigawa <88310117+AnhKhoa267@users.noreply.github.com>
2022-09-17 08:56:54 +0700

    Update baitap.cpp

--- baitap.cpp
+++ baitap.cpp
@@ -3,32 +3,15 @@
 using namespace std;
 
 void NhapMang(long a[], long& n) {
-    for (long j = 0; j < n; j++) {
-        cin >> a[j];
-    }
+    
 }
 
 void XuatMang(long a[], long n) {
-    for (long i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
+    
 }
 //f sx tang (true), sx giam (false)
 void SapXepTH(long a[], long bd, long kt, long k = true) {
-    for (long i = bd; i < kt; i++) {
-        for (long j = i + 1; j < kt; j++) {
-            if (a[i] > a[j] & k) {
-                long t = a[i];
-                a[i] = a[j];
-                a[j] = t;
-            }
-            else if (a[i] < a[j] & k == false) {
-                long t = a[i];
-                a[i] = a[j];
-                a[j] = t;
-            }
-        }
-    }
+    
 }
 bool sxgiam(long i, long j) { return (i > j); }
 bool sxtang(long i, long j) { return (i < j); }
@@ -46,19 +29,7 @@ long KetQua(long a[], long n, long k) {
 }
 
 int main() {
-    long a[1000000], n, k;
-    cin >> n >> k;
-    NhapMang(a, n);
-    /*
-    long mid = n/2;
-    SapXepTH(a, 1, n, false);
-
-    if(n%2 !=0) mid++;
-    sort(a+1, a+n, sxtang);
-    */
-    sort(a + 1, a + n, sxgiam);
-    //XuatMang(a, n);
-    cout << KetQua(a, n, k);
+    
 
     return 0;
 }
