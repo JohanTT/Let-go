8c7f922219dbe03e57f5aea682fd5dc68646ebf3
JohanTT <thejohan39@gmail.com>
2022-09-17 08:54:07 +0700

    Chinh 2

--- baitap.cpp
+++ baitap.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int main () {
+    // Thắng
     int k, n;
     int A[100];
     long sum;
-    
+
     // Ai sửa phần nào thì chú thích phần đó tên của mình với
 
     return 0;
